45dd4019661eed3903bf962cc772577e58fbba2f
Aniket Chinchankar <138643931+Aniket-Chinchankar@users.noreply.github.com>
2025-11-26 08:50:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,53 +8,49 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
-    snp = db.Column(db.Integer, primary_key=True)
+    sno = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     desc = db.Column(db.String(500), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self) -> str:
-        return f"{self.snp} - {self.title}"
+        return f"{self.sno} - {self.title}"
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        title = request.form['title']
-        desc = request.form['desc']
-        new_todo = Todo(title=title, desc=desc)
-        try:
-            db.session.add(new_todo)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return "There was an issue adding your todo"
-    else:
-        todos = Todo.query.all()  # Query all Todo items from the database
-        return render_template('index.html', todos=todos)  # Pass the todos to the template
+        title = request.form.get('title')
+        desc = request.form.get('desc')
 
-@app.route('/delete/<int:id>', methods=['POST'])
-def delete(id):
-    todo_to_delete = Todo.query.get_or_404(id)
-    try:
-        db.session.delete(todo_to_delete)
+        if not title or not desc:
+            return redirect(url_for('home'))
+
+        todo = Todo(title=title, desc=desc)
+        db.session.add(todo)
         db.session.commit()
-        return redirect('/')
-    except:
-        return "There was a problem deleting that todo"
+        return redirect(url_for('home'))
 
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-    todo = Todo.query.get_or_404(id)
+    todos = Todo.query.order_by(Todo.date_created.desc()).all()
+    return render_template('index.html', todos=todos)
+
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
+def update(sno):
+    todo = Todo.query.get_or_404(sno)
+
     if request.method == 'POST':
-        todo.title = request.form['title']
-        todo.desc = request.form['desc']
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return "There was an issue updating your todo"
-    else:
-        return render_template('update.html', todo=todo)
+        todo.title = request.form.get('title')
+        todo.desc = request.form.get('desc')
+        db.session.commit()
+        return redirect(url_for('home'))
+
+    return render_template('update.html', todo=todo)
+
+@app.route('/delete/<int:sno>', methods=['POST'])
+def delete(sno):
+    todo = Todo.query.get_or_404(sno)
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 @app.route('/products')
 def products():
@@ -65,4 +61,6 @@ def about():
     return render_template('about.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
